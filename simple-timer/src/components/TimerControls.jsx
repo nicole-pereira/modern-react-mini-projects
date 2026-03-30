838b087b1aa4d3ace7c67f484d30817f82cd055e
Nicole <npereira@confidencialimobiliario.com>
2026-03-30 13:07:58 +0100

    (Timer): Save timer to local storage

--- simple-timer/src/components/TimerControls.jsx
+++ simple-timer/src/components/TimerControls.jsx
@@ -1,8 +1,19 @@
+import { useEffect, useRef } from "react";
+
 const TimerControls = ({ isRunning, onToggle, onReset }) => {
+  const startButtonRef = useRef(null);
+
+  useEffect(() => {
+    if (startButtonRef.current) {
+      startButtonRef.current.focus();
+    }
+  }, []);
+
   return (
     <>
       <button
         onClick={onToggle}
+        ref={startButtonRef}
         className="cursor-pointer mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 mr-3">
         {isRunning ? "Pause" : "Start"}
       </button>
