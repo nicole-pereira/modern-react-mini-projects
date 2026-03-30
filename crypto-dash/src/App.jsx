9b9b47b54a301900cb596a7e29c9952cb75f1526
Nicole <npereira@confidencialimobiliario.com>
2026-03-30 16:03:26 +0100

    FilterInput Component

--- crypto-dash/src/App.jsx
+++ crypto-dash/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import CoinCard from "./components/CoinCard";
 import LimitSelector from "./components/LimitSelector";
+import FilterInput from "./components/FilterInput";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -9,6 +10,7 @@ const App = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [limit, setLimit] = useState(10);
+  const [filter, setFilter] = useState([]);
 
   const fetchCoins = async () => {
     try {
@@ -31,17 +33,29 @@ const App = () => {
     fetchCoins();
   }, [limit]);
 
+  const filteredCoins = coins.filter((coin) => {
+    return (
+      coin.name.toLowerCase().includes(filter.toString().toLowerCase()) ||
+      coin.symbol.toLowerCase().includes(filter.toString().toLowerCase())
+    );
+  });
+
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
       {loading && <p>Loading...</p>}
       {error && <div className="error">{error}</div>}
-      <LimitSelector limit={limit} onLimitChange={setLimit} />
+      <div className="top-controls">
+        <FilterInput filter={filter} onFilterChange={setFilter} />
+        <LimitSelector limit={limit} onLimitChange={setLimit} />
+      </div>
       {!loading && !error && (
         <main className="grid">
-          {coins.map((coin) => (
-            <CoinCard key={coin.id} coin={coin} />
-          ))}
+          {filteredCoins.length > 0 ? (
+            filteredCoins.map((coin) => <CoinCard key={coin.id} coin={coin} />)
+          ) : (
+            <p>No Matching Coins</p>
+          )}
         </main>
       )}
     </div>
