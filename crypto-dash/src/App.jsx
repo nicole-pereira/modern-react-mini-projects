f2e6ece84db668b0735f8fd82158b7d83256e4b2
Nicole <npereira@confidencialimobiliario.com>
2026-03-31 13:43:09 +0100

    Header component

--- crypto-dash/src/App.jsx
+++ crypto-dash/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import AboutPage from "./pages/about";
 import HomePage from "./pages/home";
+import Header from "./components/Header";
 import { Route, Routes } from "react-router";
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -34,25 +35,28 @@ const App = () => {
   }, [limit]);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <HomePage
-            coins={coins}
-            filter={filter}
-            setFilter={setFilter}
-            limit={limit}
-            setLimit={setLimit}
-            sortBy={sortBy}
-            setSortBy={setSortBy}
-            loading={loading}
-            error={error}
-          />
-        }
-      />
-      <Route path="/about" element={<AboutPage />}></Route>
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              coins={coins}
+              filter={filter}
+              setFilter={setFilter}
+              limit={limit}
+              setLimit={setLimit}
+              sortBy={sortBy}
+              setSortBy={setSortBy}
+              loading={loading}
+              error={error}
+            />
+          }
+        />
+        <Route path="/about" element={<AboutPage />}></Route>
+      </Routes>
+    </>
   );
 };
 
