7a1a0d2036af36721a881348b9724c2d8fdd20c9
Nicole <npereira@confidencialimobiliario.com>
2026-04-02 16:22:02 +0100

    ProductCard: Add to cart button

--- shopping-cart-ui/src/components/ProductCard.jsx
+++ shopping-cart-ui/src/components/ProductCard.jsx
@@ -1,4 +1,8 @@
+import { useCart } from "../context/CartContext";
+
 const ProductCard = ({ product }) => {
+  const { addToCart } = useCart();
+
   return (
     <div className="bg-white rounded-lg shadow p-4 flex flex-col">
       <img
@@ -8,6 +12,11 @@ const ProductCard = ({ product }) => {
       <h2 className="text-xl font-semibold">{product.name}</h2>
       <p className="text-grey-500 text-sm mb-2">{product.description}</p>
       <p className="font-bold text-lg">${product.price.toFixed(2)}</p>
+      <button
+        onClick={() => addToCart(product)}
+        className="bg-orange-400 text-white mt-3 px-4 py-2 cursor-pointer rounded transition hover:bg-orange-700">
+        Add to Cart
+      </button>
     </div>
   );
 };
--- shopping-cart-ui/src/context/CartContext.jsx
+++ shopping-cart-ui/src/context/CartContext.jsx
@@ -5,8 +5,22 @@ export const CartContext = createContext();
 export function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
+  const addToCart = (product) => {
+    setCart((prev) => {
+      const existing = prev.find((item) => item.id === product.id);
+      if (existing) {
+        return prev.map(() =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item,
+        );
+      }
+      return [...prev, { ...product, qty: 1 }];
+    });
+  };
+
   return (
-    <CartContext.Provider value={{ cart }}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
   );
 }
 
